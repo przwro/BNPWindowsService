56b9943b90e010db10827e40a7eefc25e9049d5e
wrobp0kr <przemyslaw.wrobel@walstead-ce.com>
2023-02-23 11:24:22 +0100

    Pierwsza działająca wersja testowa

--- BNPWindowsService/Program.cs
+++ BNPWindowsService/Program.cs
@@ -1,6 +1,15 @@
 using BNPWindowsService;
+using Serilog;
+
+var progData = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File(Path.Combine(progData, "BNPWindowsService", "serviceLog.txt"))
+    .CreateLogger();
 
 IHost host = Host.CreateDefaultBuilder(args)
+    .UseWindowsService()
+    .UseSerilog()
     .ConfigureServices(services =>
     {
         services.AddHostedService<Worker>();
--- BNPWindowsService/Worker.cs
+++ BNPWindowsService/Worker.cs
@@ -17,5 +17,17 @@ namespace BNPWindowsService
                 await Task.Delay(1000, stoppingToken);
             }
         }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("BackgroundService starting up...");
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("BackgroundService stopping!");
+            return base.StopAsync(cancellationToken);
+        }
     }
 }
